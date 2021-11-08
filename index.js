7a5f8601a9f820c6a27e5f29fbdcdb050d358093
Ulric <57965846+daulric@users.noreply.github.com>
2021-11-08 01:11:57 +0000

    I did a little update to the website

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 
+
 app.use(express.static(__dirname + "/public"), (_, res, next) => {
   res.status(404)
   res.sendFile(__dirname + "/public/404.html")
 });
+
 
 app.listen(8080);
