d5527e2dfc4dd09a523e473f200d6aaf1ceb3ddf
Ulric <57965846+daulric@users.noreply.github.com>
2021-11-08 22:31:29 +0000

    some few settings

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const app = express();
 
-
 app.use(express.static(__dirname + "/public"), (_, res, next) => {
   res.status(404)
   res.sendFile(__dirname + "/public/404.html")
 });
-
 
 app.listen(8080);
