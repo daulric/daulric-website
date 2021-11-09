1d47f7efd4b73d1e3f67fe03974f59d1970b2d73
Ulric <57965846+daulric@users.noreply.github.com>
2021-11-09 00:12:53 +0000

    few changes not much

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+//--Website Here--\\
+//404 Webpage
 app.use(express.static(__dirname + "/public"), (_, res, next) => {
   res.status(404)
   res.sendFile(__dirname + "/public/404.html")
