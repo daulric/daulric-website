fa36fb0ac16a6fb4fdcc45db29a02251b7bbde5b
Ulric <57965846+daulric@users.noreply.github.com>
2021-11-23 01:51:27 +0000

    i now get custom urls

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const express = require("express");
 const app = express();
 
 
-//Removing File Extention
+//Adding Urls for Promotions/Game
+app.get('/group',(req,res)=>res.redirect("https://www.roblox.com/groups/12852480"))
+
+//Removing File Extentions
+app.get('/policy',(req,res)=>res.sendFile(__dirname + "/public/policy.html"))
+app.get('/livestream',(req,res)=>res.sendFile(__dirname + "/public/livestream.html"))
+app.get('/team',(req,res)=>res.sendFile(__dirname + "/public/team.html"))
+app.get('/subscription',(req,res)=>res.sendFile(__dirname + "/public/subscription.html"))
+app.get('/games',(req,res)=>res.sendFile(__dirname + "/public/games.html"))
+app.get('/spotify',(req,res)=>res.sendFile(__dirname + "/public/spotify.html"))
+app.get('/status',(req,res)=>res.sendFile(__dirname + "/public/status.html"))
+app.get('/discordchat',(req,res)=>res.sendFile(__dirname + "/public/discordchat.html"))
 
 //--Website Here--\\
 //404 Webpage
