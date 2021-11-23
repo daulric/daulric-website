5c888e0019048d70a65240ebe542d63a890659a0
Ulric <57965846+daulric@users.noreply.github.com>
2021-11-23 16:20:52 +0000

    I did a little changes in the index.js express server

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 const express = require("express");
 const app = express();
 
+const publicPath = "/public"
+const adminPath = "/admin"
 
 //Adding Urls for Promotions/Game
 app.get('/group',(req,res)=>res.redirect("https://www.roblox.com/groups/12852480"))
 
-//Removing File Extentions
-app.get('/policy',(req,res)=>res.sendFile(__dirname + "/public/policy.html"))
-app.get('/livestream',(req,res)=>res.sendFile(__dirname + "/public/livestream.html"))
-app.get('/team',(req,res)=>res.sendFile(__dirname + "/public/team.html"))
-app.get('/subscription',(req,res)=>res.sendFile(__dirname + "/public/subscription.html"))
-app.get('/games',(req,res)=>res.sendFile(__dirname + "/public/games.html"))
-app.get('/spotify',(req,res)=>res.sendFile(__dirname + "/public/spotify.html"))
-app.get('/status',(req,res)=>res.sendFile(__dirname + "/public/status.html"))
-app.get('/discordchat',(req,res)=>res.sendFile(__dirname + "/public/discordchat.html"))
 
-//--Website Here--\\
+//Removing File Extentions
+app.get('/policy',(req,res)=>res.sendFile(__dirname + publicPath + "/policy.html"))
+app.get('/livestream',(req,res)=>res.sendFile(__dirname + publicPath + "/livestream.html"))
+app.get('/team',(req,res)=>res.sendFile(__dirname + publicPath + "/team.html"))
+app.get('/subscription',(req,res)=>res.sendFile(__dirname + publicPath + "/subscription.html"))
+app.get('/games',(req,res)=>res.sendFile(__dirname + publicPath + "/games.html"))
+app.get('/spotify',(req,res)=>res.sendFile(__dirname + publicPath + "/spotify.html"))
+app.get('/status',(req,res)=>res.sendFile(__dirname + publicPath + "/status.html"))
+app.get('/discordchat',(req,res)=>res.sendFile(__dirname + publicPath + "/discordchat.html"))
+////////////////////////////////////////////////////////
+
 //404 Webpage
 app.use(express.static(__dirname + "/public"), (_, res, next) => {
   res.status(404)
-  res.sendFile(__dirname + "/public/404.html")
+  res.sendFile(__dirname + publicPath + "/404.html")
 });
 
 app.listen(8080);
