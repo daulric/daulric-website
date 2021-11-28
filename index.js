a970cf16b7bfaa7424ab5073a7164f143f6cd98d
Ulric <57965846+daulric@users.noreply.github.com>
2021-11-28 17:41:28 +0000

    i made a few updates

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ const publicPath = "/public"
 
 //Adding Urls for Promotions/Game
 app.get('/group',(req,res)=>res.redirect("https://www.roblox.com/groups/12852480"))
+app.get('/blog',(req,res)=>res.redirect("https://blog.daulric.tk"))
 
 
 //Removing File Extentions
+app.get('',(req,res)=>res.sendFile(__dirname + publicPath + "/index.html"))
 app.get('/policy',(req,res)=>res.sendFile(__dirname + publicPath + "/policy.html"))
 app.get('/livestream',(req,res)=>res.sendFile(__dirname + publicPath + "/livestream.html"))
 app.get('/team',(req,res)=>res.sendFile(__dirname + publicPath + "/team.html"))
 app.get('/subscription',(req,res)=>res.sendFile(__dirname + publicPath + "/subscription.html"))
 app.get('/games',(req,res)=>res.sendFile(__dirname + publicPath + "/games.html"))
-app.get('/spotify',(req,res)=>res.sendFile(__dirname + publicPath + "/spotify.html"))
 app.get('/status',(req,res)=>res.sendFile(__dirname + publicPath + "/status.html"))
 app.get('/discordchat',(req,res)=>res.sendFile(__dirname + publicPath + "/discordchat.html"))
-////////////////////////////////////////////////////////
 
 //404 Webpage
 app.use(express.static(__dirname + "/public"), (_, res, next) => {
